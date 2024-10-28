bcbbe7f55c4de3c8b25fcc012b38c1b2a376d3d4
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-10-28 17:39:33 +0000

    entrypoint

--- logs/api/setup.py
+++ logs/api/setup.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 setup(
+    entry_points = dict(console_scripts = ['stats-api=stats.app:main']),
     install_requires = Path('requirements.txt').read_text().splitlines(),
     packages = find_packages(),
 )
