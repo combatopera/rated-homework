eb8f1675cbf84b9810a5f82bf52687909aa3d765
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-10-28 20:44:00 +0000

    return from arg

--- logs/api/stats/app.py
+++ logs/api/stats/app.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, request
 
 application = Flask(__name__)
 
 @application.route('/customers/<customer_id>/stats')
 def stats(customer_id):
-    pass
+    return request.args['from']
--- logs/get.py
+++ logs/get.py
@@ -2,6 +2,7 @@
 
 from argparse import ArgumentParser
 from pathlib import Path
+from shutil import copyfileobj
 from subprocess import check_output
 from urllib.parse import quote, quote_plus
 from urllib.request import urlopen
@@ -15,8 +16,8 @@ def main():
     container = check_output(['docker', 'compose', 'ps', '-q', 'api'], cwd = Path(__file__).parent).rstrip()
     info, = json.loads(check_output(['docker', 'inspect', container]))
     portstr, = {y['HostPort'] for x in info['NetworkSettings']['Ports'].values() for y in x}
-    with urlopen(f"http://localhost:{portstr}/customers/{quote(args.id, '')}/stats?from={quote_plus(getattr(args, 'from'))}"):
-        pass
+    with urlopen(f"http://localhost:{portstr}/customers/{quote(args.id, '')}/stats?from={quote_plus(getattr(args, 'from'))}") as f:
+        copyfileobj(f, sys.stdout.buffer)
 
 if '__main__' == __name__:
     main()
