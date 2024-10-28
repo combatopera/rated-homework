7019dd3e9a0c76a95d7381cecc86d5322387072e
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-10-28 18:58:32 +0000

    server

--- logs/api/stats/app.py
+++ logs/api/stats/app.py
@@ -1,2 +1,1 @@
-def main():
-    print('woo')
+application = 'TODO'
---
+++ logs/api/stats/server.py
@@ -0,0 +1,4 @@
+from lagoon import mod_wsgi_express
+
+def main():
+    mod_wsgi_express.start_server.__log_to_terminal.__application_type.module[exec]('stats.app')
