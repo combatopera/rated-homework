d41c1fa97c27d2612959c55ededb86a708cb9982
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-10-28 17:37:52 +0000

    sw

--- logs/api/setup.py
+++ logs/api/setup.py
@@ -2,6 +2,6 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 setup(
-    packages = find_packages(),
     install_requires = Path('requirements.txt').read_text().splitlines(),
+    packages = find_packages(),
 )
