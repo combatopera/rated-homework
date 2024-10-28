b788c085967b48213b9f211b22de5731e9fac528
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-10-28 22:22:33 +0000

    blank config

--- logs/api/stats/app.py
+++ logs/api/stats/app.py
@@ -1,14 +1,19 @@
+from aridity.config import ConfigCtrl
 from diapyr.util import singleton
 from flask import Flask, request
 
 class Application:
+
+    def __init__(self, config):
+        pass
 
     def stats(self, customer_id):
         return request.args['from']
 
 @singleton
 def application():
-    app = Application()
+    cc = ConfigCtrl()
+    app = Application(cc.r)
     flask = Flask(__name__)
     flask.route('/customers/<customer_id>/stats')(app.stats)
     return flask
