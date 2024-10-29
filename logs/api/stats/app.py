135c5edbf21ddc8df49763e334fd3291b5bb51ae
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-10-29 10:04:35 +0000

    config

--- logs/api/stats/app.py
+++ logs/api/stats/app.py
@@ -1,18 +1,21 @@
 from aridity.config import ConfigCtrl
 from diapyr.util import singleton
 from flask import Flask, request
+from psycopg import connect
 
 class Application:
 
     def __init__(self, config):
-        pass
+        self.pgpass = config.pgpass
 
     def stats(self, customer_id):
-        return request.args['from']
+        with connect(host = 'db', password = self.pgpass) as conn, conn.cursor() as cur:
+            return request.args['from']
 
 @singleton
 def application():
     cc = ConfigCtrl()
+    cc.load('config.arid')
     app = Application(cc.r)
     flask = Flask(__name__)
     flask.route('/customers/<customer_id>/stats')(app.stats)
