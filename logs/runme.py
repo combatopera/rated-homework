9b55d8a0126eb8dfb800ec421100475dbcc66e6e
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-10-29 16:05:34 +0000

    todo

--- logs/runme.py
+++ logs/runme.py
@@ -99,6 +99,7 @@ class Main:
         cc.load(configpath)
         with connect(host = 'localhost', password = cc.r.postgres.password, port = json.loads(statepath.read_bytes())['port']['db'], user = cc.r.postgres.user) as conn, conn.cursor() as cur:
             cur.execute('DROP TABLE IF EXISTS stats')
+            # TODO: Add index.
             cur.execute('CREATE TABLE stats (customer_id text NOT NULL, date date NOT NULL, successful integer NOT NULL, failed integer NOT NULL, latency_mean real NOT NULL, latency_median real NOT NULL, latency_p99 real NOT NULL)')
             days = defaultdict(Day)
             with args.logpath.open() as f:
