0c366769991e576bc622c142b78c4212ff0aa367
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-10-29 08:16:00 +0000

    i want libs

---
+++ logs/runme.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+def _equipifnecessary():
+    from pathlib import Path
+    from shutil import copy2
+    from subprocess import check_call
+    import os, sys
+    token = '--venv'
+    tokenindex = 1
+    try:
+        equipped = token == sys.argv[tokenindex]
+    except IndexError:
+        equipped = False
+    if equipped:
+        sys.argv.pop(tokenindex)
+        return
+    contextdir = Path(__file__).parent
+    requirementspath = contextdir / 'requirements.txt'
+    venvpath = contextdir / '.build' / 'venv'
+    journalpath = venvpath / 'journal'
+    try:
+        ok = journalpath.read_bytes() == requirementspath.read_bytes()
+    except FileNotFoundError:
+        ok = False
+    if not ok:
+        print('Create/update venv:', venvpath, file = sys.stderr)
+        check_call([sys.executable, '-m', 'venv', venvpath])
+        check_call([venvpath / 'bin' / 'pip', 'install', '-r', requirementspath])
+        copy2(requirementspath, journalpath)
+    args = [venvpath / 'bin' / 'python', __file__, token, *sys.argv[1:]]
+    os.execv(args[0], args)
+
+if '__main__' == __name__:
+    _equipifnecessary()
+from lagoon import docker
+import sys
+
+def main():
+    print(sys.executable, sys.argv)
+
+if '__main__' == __name__:
+    main()
