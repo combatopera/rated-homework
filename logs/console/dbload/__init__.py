f314136835ab7db27db7160024f35e5f16b281a3
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-10-30 12:27:36 +0000

    some logging

--- logs/console/dbload/__init__.py
+++ logs/console/dbload/__init__.py
@@ -3,7 +3,10 @@ from aridity.config import ConfigCtrl
 from collections import defaultdict
 from datetime import time
 from psycopg import connect
-import numpy as np, sys
+from venvpool import initlogging
+import logging, numpy as np, sys
+
+log = logging.getLogger(__name__)
 
 class Day:
 
@@ -31,19 +34,23 @@ class Day:
     def latency_percentiles(self, *percentiles):
         return np.percentile(self.durations, percentiles)
 
-def main(): # TODO: Add logging.
+def main():
+    initlogging()
     cc = ConfigCtrl()
     cc.load('config.arid')
     # XXX: Use autocommit?
     with connect(host = cc.r.postgres.host, password = cc.r.postgres.password, user = cc.r.postgres.user) as conn, conn.cursor() as cur:
         cur.execute('DROP INDEX IF EXISTS customer_date')
         cur.execute('DROP TABLE IF EXISTS daily')
+        log.info('Create table.')
         cur.execute('CREATE TABLE daily (customer_id text NOT NULL, date date NOT NULL, successful integer NOT NULL, failed integer NOT NULL, uptime real NOT NULL, latency_mean real NOT NULL, latency_median real NOT NULL, latency_p99 real NOT NULL)')
         cur.execute('CREATE UNIQUE INDEX customer_date ON daily (customer_id, date)') # XXX: Create after load?
+        log.info('Read data.')
         days = defaultdict(Day)
         for line in sys.stdin:
             isodate, isotime, customer_id, request_path, status_code, duration = line.split()
             days[customer_id, isodate].put(time.fromisoformat(isotime), int(status_code), float(duration))
+        log.info('Insert data.')
         for (customer_id, isodate), day in days.items():
             cur.execute('INSERT INTO daily VALUES (%s, %s, %s, %s, %s, %s, %s, %s)', (customer_id, isodate, day.successful, day.failed, day.uptime(), day.latency_mean(), *day.latency_percentiles(50, 99)))
 
