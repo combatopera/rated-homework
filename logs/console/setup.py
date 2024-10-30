68257873d0f0d4c54d6cba6c8ac5017bbc101522
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2024-10-30 20:33:45 +0000

    container stop with status 0

--- logs/console/setup.py
+++ logs/console/setup.py
@@ -2,7 +2,8 @@ from pathlib import Path
 from setuptools import find_packages, setup
 
 setup(
-    entry_points = dict(console_scripts = ['dbload=dbload:load', 'dbwait=dbload.wait:main', 'dbwipe=dbload:wipe']),
+    entry_points = dict(console_scripts = ['dbload=dbload:load', 'dbwait=dbload.wait:main', 'dbwipe=dbload:wipe', 'sleep-until-sigint=sleep_until_sigint:main']),
     install_requires = Path('requirements.txt').read_text().splitlines(),
     packages = find_packages(),
+    py_modules = ['sleep_until_sigint'],
 )
---
+++ logs/console/sleep_until_sigint.py
@@ -0,0 +1,8 @@
+import time
+
+def main():
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        pass
